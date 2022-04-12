1bba9a9cf9191bd318f74cab5538ef128e90bed8
Ali Mousavi <amousavig@icloud.com>
2022-04-13 03:44:22 +0430

    working on upload

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,23 @@
-﻿
+﻿// MODAL
+function showModal(modal) {
+    modal.addClass('active');
+    $('body').addClass('modal-active');
+}
+function hideModal(modal) {
+    modal.removeClass('active');
+    $('body').removeClass('modal-active');
+}
+$(document).on('click', '[data-modal]', function() {
+    var modalId = $(this).attr('data-modal');
+    showModal($('#' + modalId));
+});
+$(document).on('click', '.modal-close', function() {
+    hideModal($(this).closest('.modal'));
+});
+// HIDE MODAL ON CLICK OUTSIDE
+$(document).on('click', '.modal', function(e) {
+    if (e.target !== this) {
+        return;
+    }
+    hideModal($(this));
+});
